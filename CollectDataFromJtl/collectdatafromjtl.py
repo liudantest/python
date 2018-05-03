2ca1be847c2d4bd95776b278ef0a07193ddac69b
liudan <281170771@qq.com>
2018-05-03 23:13:21 +0800

    xxx

--- CollectDataFromJtl/collectdatafromjtl.py
+++ CollectDataFromJtl/collectdatafromjtl.py
@@ -1,3 +1,17 @@
+import linecache
 
-file=open('test.txt','r')
-print(file.read())
+the_line=linecache.getline('test',222)
+print(the_line)
+
+def getline(the_file_path,line_number):
+    if linecache<1:
+        return ''
+    for cur_line_number,line in enumerate(open(the_file_path,'rU')):
+        if cur_line_number==line_number-1:
+            return line
+    return ''
+
+
+if __name__ == '__main__':
+    the_line=linecache.getline('test',222)
+    print(the_line)
