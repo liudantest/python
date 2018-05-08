23a97ddc4d7505fe669f5f4d65b8b5a5229f52e6
suhui <dan.liu@qingtui.cn>
2018-05-08 13:55:26 +0800

    commit collectdatafromjtl

--- CollectDataFromJtl/collectdatafromjtl.py
+++ CollectDataFromJtl/collectdatafromjtl.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
 import linecache
 import xlwt
+import os
 
 new_book = xlwt.Workbook()
 new_sheet = new_book.add_sheet("sheet1")
-new_sheet.write(0,0,"tps")
-new_sheet.write(0,1,"avg")
-new_sheet.write(0,2,"min")
-new_sheet.write(0,3,"max")
-new_sheet.write(0,4,"err")
+new_sheet.write(0,0,"name")
+new_sheet.write(0,1,"thread")
+new_sheet.write(0,2,"tps")
+new_sheet.write(0,3,"avg")
+new_sheet.write(0,4,"min")
+new_sheet.write(0,5,"max")
+new_sheet.write(0,6,"err")
 
 #获取txt文件的行数
 def get_line_count2(filename):
@@ -19,32 +24,39 @@ def get_line_count2(filename):
     print(count)
     return count
 #获取指定行中的指定内容
-def get_result(content):
+def insert_result(content,row,file):
 
     str=content.split('=')[2].strip(" ")
 
     print("str:",str)
 
-    tps=str.split('/s')[0]
-    print("tps:",tps)
+    tps = str.split('/s')[0]
+    print("tps:", tps)
 
-    avg=str.split(' ')[5]
-    print("avg:",avg)
+    avg = str.split(' ')[3]
+    print("avg:", avg)
 
-    min=str.split(' ')[11]
-    print("min:",min)
+    min = str.split(' ')[7]
+    print("min:", min)
 
-    max =str.split(' ')[14]
-    print("max:",max)
+    max = str.split(' ')[10]
+    print("max:", max)
 
-    err =str.split(' ')[18]
-    print("err:",err)
+    err = str.split('Err:     ')[1]
+    print("err:", err)
 
-    new_sheet.write(1, 0, tps)
-    new_sheet.write(1, 1, avg)
-    new_sheet.write(1, 2, min)
-    new_sheet.write(1, 3, max)
-    new_sheet.write(1, 4, err)
+    name=file.split('.')[0]
+    print(name)
+
+    thread=file.split('.')[1]
+    print(thread)
+    new_sheet.write(row, 0, name)
+    new_sheet.write(row, 1, thread)
+    new_sheet.write(row, 2, tps)
+    new_sheet.write(row, 3, avg)
+    new_sheet.write(row, 4, min)
+    new_sheet.write(row, 5, max)
+    new_sheet.write(row, 6, err)
 
     return tps,avg,min,max,err
 
@@ -57,22 +69,43 @@ def creat_excel():
 
 
 #获取倒数第三行的数据
-def get_tps(line_number):
-    the_line=linecache.getline('test.txt',222)
+def get_rowdata(line_number,file):
+    the_line=linecache.getline(file,222)
     print(the_line)
     if line_number<1:
         return ''
-    for cur_line_number,line in enumerate(open('test.txt','rU')):
+    for cur_line_number,line in enumerate(open(file,'rU')):
         if cur_line_number==line_number-1:
             return line
     return ''
 
 
+# 遍历指定目录，显示目录下的所有文件名
+def eachFile(filepath):
+    pathDir =  os.listdir(filepath)
+    path=set()
+    for allDir in pathDir:
+        child = os.path.join('%s%s' % (filepath, allDir))
+        #print(child) # .decode('gbk')是解决中文显示乱码问题
+        path.add(child)
+    return path
+
 
 if __name__ == '__main__':
-    line_number=get_line_count2('test.txt')
-    the_line=get_tps(line_number-2)
-    get_result(the_line)
-    new_book.save("NewCreateWorkbook.xls")
+    child=eachFile('E:\PythonTools\CollectDataFromJtl\data\\')
+
+    a=1
+    for file in child:
+        print("test>>>",file)
+        line_number=get_line_count2(file)
+        the_line=get_rowdata(line_number-2,file)
+        insert_result(the_line,a,file)
+        a+=1
+        new_book.save("NewCreateWorkbook.xls")
+
+
+
+
+
 
 
